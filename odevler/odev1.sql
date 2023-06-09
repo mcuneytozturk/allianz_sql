0771734d4dcad272c76320939024b7e6366de48e
Muhsin Cüneyt ÖZTÜRK <mcuneytozturkc@gmail.com>
2023-06-09 14:15:02 +0300

    odev1 düzeltme yap

--- odevler/odev1.sql
+++ odevler/odev1.sql
@@ -15,4 +15,4 @@ WHERE first_name = 'Mary';
 --5.senaryo
 SELECT *
 FROM film
-WHERE NOT (length > 50 AND (rental_rate = 2.99 OR rental_rate = 4.99));
+WHERE NOT (length > 50) AND NOT(rental_rate = 2.99 OR rental_rate = 4.99));
